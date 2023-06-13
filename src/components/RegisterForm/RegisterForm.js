c6eb7bff02665f8ae8770cc90034fd56c23e173e
BohdanMus <bogdanmusienko@gmail.com>
2023-06-13 16:21:40 +0300

    Update RegisterForm.js

--- src/components/RegisterForm/RegisterForm.js
+++ src/components/RegisterForm/RegisterForm.js
@@ -36,35 +36,7 @@ export const RegisterForm = () => {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
 
-  // const handleSubmit = e => {
-  //   e.preventDefault();
-  //   const form = e.currentTarget;
-  //   dispatch(
-  //     register({
-  //       name: form.elements.name.value,
-  //       email: form.elements.email.value,
-  //       password: form.elements.password.value,
-  //     })
-  //   );
-  //   form.reset();
-  // };
-
   return (
-    // <form onSubmit={handleSubmit} autoComplete="off">
-    //   <label>
-    //     Username
-    //     <input type="text" name="name" />
-    //   </label>
-    //   <label>
-    //     Email
-    //     <input type="email" name="email" />
-    //   </label>
-    //   <label>
-    //     Password
-    //     <input type="password" name="password" />
-    //   </label>
-    //   <button type="submit">Register</button>
-    // </form>
     <>
       {isLoading ? (
         <PageLoader />
@@ -92,7 +64,6 @@ export const RegisterForm = () => {
             const isErrorEmail = !props.values.email && props.touched.email;
             const isErrorPassword =
               !props.values.password && props.touched.password;
-
             return (
               <Form>
                 <Field name="name">
@@ -112,7 +83,7 @@ export const RegisterForm = () => {
                     </FormControl>
                   )}
                 </Field>
-                <ErrorMessage name="name" component="div" />
+                <ErrorMessage name="name" component="span" />
                 <Field name="email">
                   {({ field }) => (
                     <FormControl isRequired isInvalid={isErrorEmail} mb={3}>
@@ -130,7 +101,7 @@ export const RegisterForm = () => {
                     </FormControl>
                   )}
                 </Field>
-                <ErrorMessage name="email" component="div" />
+                <ErrorMessage name="email" component="span" />
                 <Field name="password">
                   {({ field }) => (
                     <FormControl isRequired isInvalid={isErrorPassword}>
@@ -169,7 +140,7 @@ export const RegisterForm = () => {
                     </FormControl>
                   )}
                 </Field>
-                <ErrorMessage name="password" component="div" />
+                <ErrorMessage name="password" component="span" />
                 <Button
                   leftIcon={<HiOutlinePencilSquare />}
                   mt={4}
