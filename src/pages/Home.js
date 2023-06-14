194a4b57d53d50526872705d5b9dbe05ed29bc99
BohdanMus <bogdanmusienko@gmail.com>
2023-06-15 01:11:22 +0300

    edit Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,16 @@
-import { Title, Text } from './Home.styled';
+import { Title, UnderTitle, HomeLink } from './Home.styled';
 
 const Home = () => {
   return (
     <section>
       <Title>Welcome to Phonebook!</Title>
-      <Text>Create your own personal book of contacts</Text>
+      <UnderTitle>
+        Please
+        <HomeLink to="/register">Register</HomeLink>
+        or
+        <HomeLink to="/login">Log in</HomeLink>
+        to be able to use your PhoneBook
+      </UnderTitle>
     </section>
   );
 };
--- src/pages/Home.styled.js
+++ src/pages/Home.styled.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Title = styled.h2`
   font-size: 32px;
@@ -18,4 +19,25 @@ export const Text = styled.p`
   @media screen and (min-width: 320px) and (max-width: 767px) {
     font-size: 16px;
   }
+`;
+
+export const HomeLink = styled(Link)`
+  text-decoration: none;
+  margin: 0 6px;
+  color: white;
+  transition: all 0.2s cubic-bezier(0.645, 0.045, 0.355, 1);
+  border-radius: 8px;
+  padding: 5px 8px;
+  background-color: #239b56;
+  font-weight: 400;
+  font-size: 20px;
+  :hover {
+    opacity: 0.8;
+  }
+`;
+export const UnderTitle = styled.h2`
+  color: black;
+  text-align: center;
+  font-size: 28px;
+  height: 40px;
 `;
